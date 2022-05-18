c8916c7b679c78453c0243d06c05e7d10c4362c5
aiden <zero2one23581@gmail.com>
2022-05-18 11:10:47 +0900

    ✨ 아이디찾기 - 인증토큰 생성

--- pages/api/user/login/findId.ts
+++ pages/api/user/login/findId.ts
@@ -4,8 +4,36 @@ import withHandler from '../../../../src/libs/server/withHandler';
 import { withApiSession } from '../../../../src/libs/server/withSession';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { user } = req.session;
+  if (user) return res.json({ ok: false, error: 'YOU MUST SIGN OUT!' });
+  const tokenNum = Math.floor(Math.random() * 90000) + 10000; //6 random digits
   const email = req.body;
-  console.log(email);
+  //
+  if (!email)
+    return res.json({ ok: false, error: '데이터가 미입력 되었습니다.' });
+  //
+  const foundUser = await prismaClient.user.findUnique({
+    where: { email },
+    select: { id: true, email: true },
+  });
+  if (!foundUser)
+    return res.json({ ok: false, error: '등록되지 않은 이메일 입니다.' });
+
+  //토큰생성
+  const token = await prismaClient.token.create({
+    data: {
+      tokenNum,
+      user: {
+        connect: { id: foundUser.id },
+      },
+    },
+  });
+  if (!token) return res.json({ ok: false, error: 'FAIL TO CREATE TOKEN' });
+
+  //이메일로 토큰숫자 전달 (나중에 구현)
+
+  //
+  return res.json({ ok: true });
 }
 
 export default withApiSession(
