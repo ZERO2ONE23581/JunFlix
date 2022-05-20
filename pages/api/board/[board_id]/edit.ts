76e4e944e7d3ed3abba20cf8584d1aac6172b1c9
aiden <zero2one23581@gmail.com>
2022-05-20 13:44:36 +0900

    ✅ Edit Board Done

--- pages/api/board/[board_id]/edit.ts
+++ pages/api/board/[board_id]/edit.ts
@@ -6,17 +6,42 @@ import { withApiSession } from '../../../../src/libs/server/withSession';
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { user } = req.session;
   const { board_id } = req.query;
-  const data = req.body;
+  const { titleCap, genre, intro } = req.body;
+  const noInput = !Boolean(titleCap && genre && intro);
 
   //error handling
-  if (!user) return res.json({ ok: false, error: 'LOGIN NEEDED' });
-  if (!board_id) return res.json({ ok: false, error: 'URL ERROR' });
+  if (!user) return res.json({ ok: false, error: 'LOGIN NEEDED!' });
+  if (!board_id) return res.json({ ok: false, error: 'URL ERROR!' });
+  if (noInput) return res.json({ ok: false, error: 'NO INPUT DATA!' });
 
   //Select board
-  const board = await prismaClient.board.findUnique({
+  const foundBoard = await prismaClient.board.findUnique({
     where: { id: +board_id.toString() },
+    select: { id: true, UserID: true, title: true },
+  });
+  if (!foundBoard) return res.json({ ok: false, error: 'NO BOARD FOUND!' });
+
+  //Only creator can edit his board!
+  if (foundBoard?.UserID !== user?.id)
+    return res.json({ ok: false, error: '보드의 작성자가 아닙니다.' });
+
+  //중복된 제목 체크
+  const dupTitle = Boolean(
+    await prismaClient.board.findUnique({
+      where: { title: titleCap },
+    })
+  );
+  console.log('titleCap==', titleCap);
+  console.log('foundBoard.title==', foundBoard.title);
+  if (dupTitle && titleCap !== foundBoard.title)
+    return res.json({ ok: false, error: '이미 사용중인 제목입니다.' });
+
+  //Edit board
+  await prismaClient.board.update({
+    where: { id: foundBoard.id },
+    data: { title: titleCap, genre, intro },
   });
   //
-  return res.json({ ok: true, board });
+  return res.json({ ok: true, message: '보드가 수정되었습니다.' });
 }
 export default withApiSession(withHandler({ methods: ['POST'], handler }));
--- pages/user/[userId]/board/[boardId]/index.tsx
+++ pages/user/[userId]/board/[boardId]/index.tsx
@@ -8,12 +8,12 @@ import useSWR from 'swr';
 import { Btn } from '../../../../../src/components/Btn';
 import { Input, Select } from '../../../../../src/components/Input';
 import useMutation from '../../../../../src/libs/client/useMutation';
+import { PostResponse } from '../../../../../src/types/postResponse';
 import {
   Article,
   BoardPage,
-  Flex,
-  Form,
-  PageContainer,
+  ErrMsg,
+  OkMsg,
 } from '../../../../../styles/components/default';
 
 interface IBoardRes {
@@ -32,10 +32,9 @@ const myBoard: NextPage = () => {
   const { data } = useSWR<IBoardRes>(`/api/board/${Number(boardId)}`);
 
   //Post
-  const [editBoard, { data: editedData, loading }] = useMutation(
+  const [editBoard, { data: editedData, loading }] = useMutation<PostResponse>(
     `/api/board/${Number(boardId)}/edit`
   );
-
   //Edit Form
   const {
     register,
@@ -44,9 +43,10 @@ const myBoard: NextPage = () => {
     formState: { errors },
   } = useForm<IEditBoardForm>({ mode: 'onSubmit' });
 
-  const onValid = (formData: IEditBoardForm) => {
+  const onValid = ({ title, genre, intro }: IEditBoardForm) => {
     if (loading) return;
-    editBoard(formData);
+    const titleCap = title?.toUpperCase();
+    editBoard({ titleCap, genre, intro });
   };
   const [edit, setEdit] = useState(false);
   const onClick = () => {
@@ -57,7 +57,12 @@ const myBoard: NextPage = () => {
     if (data?.board?.title) setValue('title', data.board.title.toUpperCase());
     if (data?.board?.genre) setValue('genre', data.board.genre);
     if (data?.board?.intro) setValue('intro', data.board.intro);
-  }, [setValue, data]);
+    // if (editedData?.ok) setEdit((p) => !p);
+    if (editedData?.ok)
+      setTimeout(() => {
+        router.reload();
+      }, 1000);
+  }, [setValue, data, editedData]);
   //
   return (
     <>
@@ -69,6 +74,8 @@ const myBoard: NextPage = () => {
               type="yesOrno"
               btnName={edit ? 'Back' : 'Edit'}
             />
+            {editedData?.message && <OkMsg>{editedData?.message}</OkMsg>}
+            {editedData?.error && <ErrMsg>{editedData?.error}</ErrMsg>}
             <form onSubmit={handleSubmit(onValid)}>
               <Input
                 errMsg={errors.title?.message}
