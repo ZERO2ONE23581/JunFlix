439f09ca9c248297a9c99af417df806155ae9097
aiden <zero2one23581@gmail.com>
2022-05-24 10:38:45 +0900

    ✨ All ratings get API

--- pages/api/review/rating/all_ratings.ts
+++ pages/api/review/rating/all_ratings.ts
@@ -4,16 +4,21 @@ import withHandler from '../../../../src/libs/server/withHandler';
 import { withApiSession } from '../../../../src/libs/server/withSession';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  //Get all reviews -> ratings
-  const allReviews = await client.review.findMany({
-    include: { user: { select: { username: true } } },
+  //Get all reviews -> all ratings
+  const allRatings = await client.review.findMany({
+    select: {
+      score: true,
+      oneline: true,
+      recommend: true,
+      user: { select: { username: true } },
+    },
     orderBy: {
       id: 'desc',
     },
   });
-  if (!allReviews) return res.json({ ok: false, error: 'NO REVIEWS!' });
+  if (!allRatings) return res.json({ ok: false, error: 'NO RATINGS!' });
   //
-  return res.json({ ok: true, allReviews });
+  return res.json({ ok: true, allRatings });
 }
 export default withApiSession(
   withHandler({ methods: ['GET'], handler, isPrivate: false })
